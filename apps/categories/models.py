3d3cba45a6a85401cbedd4fa4b1e4a75133fbb40
Toktorov <ktoktorov2005@gmail.com>
2021-10-23 15:14:26 +0600

    add category model

---
+++ apps/categories/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.db.models.signals import pre_save
+from utils.slug_generator import unique_slug_generators
+
+# Create your models here.
+class Category(models.Model):
+    title = models.CharField(
+        max_length=255,
+        verbose_name='Наименование'
+    )
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        related_name='children',
+        null=True, blank=True
+    )
+    slug = models.SlugField(blank=True, unique=True)
+
+    def __str__(self):
+        return f"{self.title} -- {self.slug}"
+
+
+def slag_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generators(instance)
+
+
+pre_save.connect(slag_pre_save_receiver, sender=Category)
