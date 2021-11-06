198477fe18abf3874ff4c0ae4ee342709982cff3
Toktorov <ktoktorov2005@gmail.com>
2021-11-06 15:59:23 +0600

    some changes

--- DjangoStore/urls.py
+++ DjangoStore/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.categories.urls')),
-    path('', include('apps.products.urls'))
+    path('', include('apps.products.urls')),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
