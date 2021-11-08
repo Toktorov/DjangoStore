a787964ed8cbe860acb016826c2150cbe2e1affa
Toktorov <ktoktorov2005@gmail.com>
2021-11-08 19:23:51 +0600

    add google login

--- DjangoStore/settings.py
+++ DjangoStore/settings.py
@@ -112,6 +112,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+]
+
+
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
